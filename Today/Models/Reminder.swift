6327aae747fd16fc28add104b7150b6e97abac3a
Nortiz <nortiz1349@gmail.com>
2022-09-07 10:45:13 +0900

    List View Created

---
+++ Today/Models/Reminder.swift
@@ -0,0 +1,34 @@
+//
+//  Reminder.swift
+//  Today
+//
+//  Created by Nortiz M1 on 2022/09/06.
+//
+
+import Foundation
+
+struct Reminder {
+	var title: String
+	var dueDate: Date
+	var notes: String? = nil
+	var isComplete: Bool = false
+}
+
+
+// #If DEBUG 플래그는 출시를 위해 앱을 빌드할 때 동봉된 코드가 컴파일되는 것을 방지하는 컴파일 지시문입니다.
+// 이 플래그를 사용하여 디버그 빌드에서 코드를 테스트하거나 다음 단계에서와 같이 샘플 테스트 데이터를 제공할 수 있습니다.
+#if DEBUG
+extension Reminder {
+	static var sampleData = [
+		Reminder(title: "Submit reimbursement report", dueDate: Date().addingTimeInterval(800.0), notes: "Don't forget about taxi receipts"),
+		Reminder(title: "Code review", dueDate: Date().addingTimeInterval(14000.0), notes: "Check tech specs in shared folder", isComplete: true),
+		Reminder(title: "Pick up new contacts", dueDate: Date().addingTimeInterval(24000.0), notes: "Optometrist closes at 6:00PM"),
+		Reminder(title: "Add notes to retrospective", dueDate: Date().addingTimeInterval(3200.0), notes: "Collaborate with project manager", isComplete: true),
+		Reminder(title: "Interview new project manager candidate", dueDate: Date().addingTimeInterval(60000.0), notes: "Review portfolio"),
+		Reminder(title: "Mock up onboarding experience", dueDate: Date().addingTimeInterval(72000.0), notes: "Think different"),
+		Reminder(title: "Review usage analytics", dueDate: Date().addingTimeInterval(83000.0), notes: "Discuss trends with management"),
+		Reminder(title: "Confirm group reservation", dueDate: Date().addingTimeInterval(92500.0), notes: "Ask about space heaters"),
+		Reminder(title: "Add beta testers to TestFlight", dueDate: Date().addingTimeInterval(101000.0),  notes: "v0.9 out on Friday")
+	]
+}
+#endif
